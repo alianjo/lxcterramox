119def33027333ead58c70df36ebacb5d9816be8
Ali anjo <69678339+alianjo@users.noreply.github.com>
2021-11-12 15:05:46 +0330

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "2.9.0"
+    }
+  }
+}
+
+provider "proxmox" {
+    pm_api_url = "https://<Proxmox-IP>:8006/api2/json"
+    pm_debug = true
+    pm_user = "<USER>@pve"
+    pm_password = "<PASSWORD>"
+}
+
+######### LXC container
+
+resource "proxmox_lxc" "basic" {
+  target_node  = "pve"
+  hostname     =  "instance-${ count.index + 1 }"
+  ostemplate   = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  password     = "<PASSWORD>"  # this password is for user login the default user is root
+  unprivileged = true
+  start        = true
+  count        = 3        # how many users you want to create (you can change it)
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.1.1${count.index + 1}/24" #we are starting the ip addresses from 192.168.1.10 you can change it (the prefix is necessary)
+  }
+}
